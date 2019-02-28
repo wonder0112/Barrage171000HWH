8a1a6aafb7fe0e2f270d0e679126d18dde869a9b
wonder0112 <hwhgdcn@gmail.com>
2019-02-28 15:30:55 +0800

    视频播放，及循环播放

--- app/src/main/java/cn/edu/gdpt/xxgcx/barrage171000hwh/MainActivity.java
+++ app/src/main/java/cn/edu/gdpt/xxgcx/barrage171000hwh/MainActivity.java
@@ -1,5 +1,7 @@
 package cn.edu.gdpt.xxgcx.barrage171000hwh;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
@@ -24,8 +26,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+        playVideo();
     }
 
+    /**
+     * 播放视频
+     */
+    private void playVideo(){
+        String uri="android.resource://"+getPackageName()+"/"+R.raw.sun;//视频路径
+        if(uri!=null){
+            mVdoViMainVideo.setVideoURI(Uri.parse(uri));//通过URI解析路径，并赋给VideoView控件
+            mVdoViMainVideo.start();//播放视频
+        }else {
+            mVdoViMainVideo.getBackground().setAlpha(0);//设定背景透明
+        }
+        //循环播放
+        mVdoViMainVideo.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.start();
+                mp.setLooping(true);
+            }
+        });
+    }
     private void initView() {
         mVdoViMainVideo = (VideoView) findViewById(R.id.vdoVi_main_video);
         mKuViewMainBarrage = (DanmakuView) findViewById(R.id.kuView_main_barrage);
@@ -35,26 +58,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         mBtnMainSend.setOnClickListener(this);
     }
-
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btn_main_send:
-
+                submit();
                 break;
         }
     }
-
     private void submit() {
         // validate
         String input = mEdtTxtMainInput.getText().toString().trim();
         if (TextUtils.isEmpty(input)) {
-            Toast.makeText(this, "input不能为空", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "不能为空", Toast.LENGTH_SHORT).show();
             return;
         }
-
         // TODO validate success, do something
-
-
     }
 }
